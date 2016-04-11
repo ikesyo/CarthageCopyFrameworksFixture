b56c6cf8fb480fbb5c6dbdcf5d1f71bb9af21f36
Syo Ikeda <suicaicoca@gmail.com>
2016-04-11 20:56:31 +0900

    Initial commit

---
+++ CarthageCopyFrameworksFixture/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CarthageCopyFrameworksFixture
+//
+//  Created by Syo Ikeda on 4/11/16.
+//  Copyright © 2016 Syo Ikeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
